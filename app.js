66bfcf6c3ed0a4e661c2b36ff2c0026f14e2afee
Sai-Krishna-Web <60420794+Sai-Krishna-Web@users.noreply.github.com>
2021-04-24 19:34:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const acceptButton = document.querySelector(".accept");
+const rejectButton = document.querySelector(".reject");
+const description = document.querySelector(".info h3");
+//const sizes = document.querySelector(".sizes");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  //sizes.style.transform = "translateZ(100px)";
+  acceptButton.style.transform = "translateZ(75px)";
+  rejectButton.style.transform = "translateZ(75px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  //sizes.style.transform = "translateZ(0px)";
+  acceptButton.style.transform = "translateZ(0px)";
+  rejectButton.style.transform = "translateZ(75px)";
+});
+
+function accept() {
+  document.getElementById("sorryText").style.display = "none"
+  document.getElementById("reject").style.display = "none"
+  document.getElementById("accept").style.display = "none"
+  document.getElementById("img").style.display = "none"
+  document.getElementById("thankYou").style.display = "block"
+  document.getElementById("response").innerHTML = "Thank you, It means a lot to me...🙂";
+}
+
+function reject() {
+  document.getElementById("sorryText").style.display = "none"
+  document.getElementById("reject").style.display = "none"
+  document.getElementById("response").innerHTML = "I'm really really sorry, please accept my apologies...🥺";
+}
